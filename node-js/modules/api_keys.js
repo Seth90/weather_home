d244ffc162a914b78e1498aced0f008e166f984d
Seth90 <seth_90@mail.ru>
2023-04-03 15:14:36 +0300

    Update api_keys.js

--- node-js/modules/api_keys.js
+++ node-js/modules/api_keys.js
@@ -1,5 +1,5 @@
 const API_KEYS = {
-  yandex: '492c7161-94e4-407b-8e3a-b93feea2bf44',
-  accuweather: 'I7OgkSDcK6z1IqXQ0dU0WXXjbSPhgbrv'};
+  yandex: '',
+  accuweather: ''};
 
   module.exports = API_KEYS;
